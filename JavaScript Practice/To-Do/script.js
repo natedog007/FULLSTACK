655364aba58added0e1132ab525188acf420e60d
Nathan Boyden <nrb121903@gmail.com>
2025-12-18 12:26:37 -0500

    ok

---
+++ JavaScript Practice/To-Do/script.js
@@ -0,0 +1,33 @@
+
+let choice = "default";
+let ToDoList = [];
+
+while (choice != "quit"){
+    choice = prompt("What would you like to do?");
+    
+    if(choice == "new"){
+        let todoItem = prompt("Enter new To-Do");
+        ToDoList.push(todoItem);
+    }
+    else if(choice == "list"){
+        for(let i = 0; i < ToDoList.length; i++){
+            console.log(`${i} : ${ToDoList[i]}`);
+        }
+    }
+    else if(choice == "delete"){
+        let deleteItem = parseInt(prompt("What would you like to delete?"));
+        ToDoList.pop(deleteItem);
+        console.log("To-Do Removed");
+    }
+    else if(choice == "quit"){
+        continue;
+    }
+    else{
+        choice = prompt("Please Enter valid option. Type 'ok' ");
+    }
+    
+
+}
+
+
+
